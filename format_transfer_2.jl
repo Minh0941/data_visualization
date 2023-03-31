408e15f4147c0f3451e680bdc144a8d1ced078e3
staruntracer <125101932+staruntracer@users.noreply.github.com>
2023-03-30 20:20:46 -0700

    Add files via upload

--- format_transfer_2.jl
+++ format_transfer_2.jl
@@ -3,30 +3,43 @@ using JSON
 
 function generate_edge_bundling_json(csv_file::AbstractString, json_path::AbstractString)
     # read data from CSV file
-    data = CSV.read(csv_file)
+    data = DataFrame(CSV.File(csv_file))
 
     # create dictionary to store source outcomes for each unique key
     source_outcomes = Dict{String, Vector{String}}()
-
+     
     # iterate over rows in data
     for row in data
-        # concatenate first three columns to create unique key
-        if any(ismissing, row)
-            # skip row if it contains empty data
-            continue
-    
-        key = join(row[1:3])
+        code = row.Courses * row.LO
+        if(isempty(source_outcomes)) #if dict is empty
+            temp= source_outcome(row.["Course Prerequisite"],row.LOP,row.["LO ID"])
+            source_outcomes[code]=[temp]
+        else # the dict has at least one entry
+            if !haskey(transcript_courses,code) # current course and LO is not in dict
 
-        # concatenate fourth and fifth columns to create source outcome
-        source_outcome = join(row[4:5])
-
-        # add source outcome to values array for corresponding key
-        if haskey(source_outcomes, key)
-            push!(source_outcomes[key], source_outcome)
-        else
-            source_outcomes[key] = [source_outcome]
+                if ';' in row.LOP      
+                    strings = split(row.LOP,";")
+                    temp_array=[strings[1],strings[2]]
+                    temp= source_outcome(row.["Course Prerequisite"],temp_array,row.["LO ID"]) 
+                else
+                temp= source_outcome(row.["Course Prerequisite"],row.LOP,row.["LO ID"]) 
+                end
+                source_outcomes[code]=[temp]
+            else
+                if ';' in row.LOP      
+                    strings = split(row.LOP,";")
+                    temp_array=[strings[1],strings[2]]
+                    temp= source_outcome(row.["Course Prerequisite"],temp_array,row.["LO ID"]) 
+                else
+                temp= source_outcome(row.["Course Prerequisite"],row.LOP,row.["LO ID"]) 
+                end
+                push!(source_outcomes[code],temp)
+                
+            end
         end
     end
+
+
 
     # save source outcomes dictionary as JSON file
     json_file = json_path * ".json"
